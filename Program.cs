9d080a475b00cbf399b1a94d9dca9ed0f32fa8c2
Laurence Luo <admin@lzc.app>
2024-06-03 22:33:21 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,32 +6,40 @@
             var writer = new StreamWriter("gps_data.geojson");
 
             writer.WriteLine("{");
-            writer.WriteLine("    \"type\": \"FeatureCollection\",");
-            writer.WriteLine("    \"features\": [");
+            writer.WriteLine("\t\"type\": \"FeatureCollection\",");
+            writer.WriteLine("\t\"features\": [");
 
-            for (var i = 1; i < lines.Length; i++) {
+            for (var i = 1; i < lines.Length; i++)
+            {
                 var columns = lines[i].Split(',');
 
                 var latitude = Convert.ToDouble(columns[0]);
                 var longitude = Convert.ToDouble(columns[1]);
-                var timestamp = Convert.ToInt64(columns[2]);
+                var altitude = Convert.ToDecimal(columns[2]);
+                var timestamp = Convert.ToInt64(columns[3]);
 
-                writer.WriteLine("        {");
-                writer.WriteLine("            \"type\": \"Feature\",");
-                writer.WriteLine("            \"geometry\": {");
-                writer.WriteLine("                \"type\": \"Point\",");
-                writer.WriteLine("                \"coordinates\": [");
-                writer.WriteLine("                    " + longitude + ",");
-                writer.WriteLine("                    " + latitude);
-                writer.WriteLine("                ]");
-                writer.WriteLine("            },");
-                writer.WriteLine("            \"properties\": {");
-                writer.WriteLine("                \"timestamp\": " + timestamp);
-                writer.WriteLine("            }");
-                writer.WriteLine("        },");
+                writer.WriteLine("\t\t{");
+                writer.WriteLine("\t\t\t\"type\": \"Feature\",");
+                writer.WriteLine("\t\t\t\"geometry\": {");
+                writer.WriteLine("\t\t\t\t\"type\": \"Point\",");
+                writer.WriteLine("\t\t\t\t\"coordinates\": [");
+                writer.WriteLine("\t\t\t\t\t" + longitude + ",");
+                writer.WriteLine("\t\t\t\t\t" + latitude);
+                writer.WriteLine("\t\t\t\t]");
+                writer.WriteLine("\t\t\t},");
+                writer.WriteLine("\t\t\t\"properties\": {");
+                writer.WriteLine("\t\t\t\t\"altitude\": " + altitude + ",");
+                writer.WriteLine("\t\t\t\t\"timestamp\": " + timestamp);
+                writer.WriteLine("\t\t\t}");
+
+                if (i != lines.Length - 1) {
+                    writer.WriteLine("\t\t},");
+                } else {
+                    writer.WriteLine("\t\t}");
+                }
             }
 
-            writer.WriteLine("    ]");
+            writer.WriteLine("\t]");
             writer.WriteLine("}");
 
             writer.Flush();
